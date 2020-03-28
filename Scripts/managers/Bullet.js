8f9a55a15b62431a6e345bbde247220bd4a8afaa
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-03-28 14:50:05 -0400

    added Bullet Manager

---
+++ Scripts/managers/Bullet.js
@@ -0,0 +1,45 @@
+"use strict";
+var managers;
+(function (managers) {
+    var Bullet = /** @class */ (function () {
+        // PUBLIC PROPERTIES
+        // CONSTRUCTOR
+        function Bullet() {
+            this._buildBulletPool();
+        }
+        // PRIVATE METHODS
+        Bullet.prototype._buildBulletPool = function () {
+            // initialize bullet number
+            this._bulletNumber = 50;
+            // create an empty container
+            this._bulletPool = new Array();
+            for (var count = 0; count < this._bulletNumber; count++) {
+                var bullet = new objects.Bullet();
+                this._bulletPool.push(bullet);
+            }
+        };
+        // PUBLIC METHODS
+        Bullet.prototype.AddBulletsToScene = function (scene) {
+            this._bulletPool.forEach(function (bullet) {
+                scene.addChild(bullet);
+            });
+        };
+        Bullet.prototype.GetBullet = function () {
+            // remove the bullet from the front of the pool
+            var bullet = this._bulletPool.shift();
+            bullet.isActive = true;
+            // push the bullet to the back of the pool
+            this._bulletPool.push(bullet);
+            // return a reference to the active bullet
+            return bullet;
+        };
+        Bullet.prototype.Update = function () {
+            this._bulletPool.forEach(function (bullet) {
+                bullet.Update();
+            });
+        };
+        return Bullet;
+    }());
+    managers.Bullet = Bullet;
+})(managers || (managers = {}));
+//# sourceMappingURL=Bullet.js.map
---
+++ Scripts/managers/Bullet.ts
@@ -0,0 +1,64 @@
+module managers
+{
+    export class Bullet 
+    {
+        // PRIVATE INSTANCE MEMBERS
+        private _bulletNumber: number;
+        private _bulletPool: Array<objects.Bullet>;
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTOR
+        constructor()
+        {
+
+            this._buildBulletPool();
+        }
+
+        // PRIVATE METHODS
+        private _buildBulletPool():void
+        {
+            // initialize bullet number
+            this._bulletNumber = 50;
+
+            // create an empty container
+            this._bulletPool = new Array<objects.Bullet>();
+
+            for (let count = 0; count < this._bulletNumber; count++) {
+                let bullet = new objects.Bullet();
+                this._bulletPool.push(bullet);
+            }
+        }
+
+        // PUBLIC METHODS
+
+        public AddBulletsToScene(scene:objects.Scene)
+        {
+            this._bulletPool.forEach(bullet => {
+                scene.addChild(bullet);
+            });
+        }
+
+
+        public GetBullet():objects.Bullet
+        {
+            // remove the bullet from the front of the pool
+            let bullet = this._bulletPool.shift();
+
+            bullet.isActive = true;
+
+            // push the bullet to the back of the pool
+            this._bulletPool.push(bullet);
+
+            // return a reference to the active bullet
+            return bullet;
+        }
+
+        public Update()
+        {
+            this._bulletPool.forEach(bullet => {
+                bullet.Update();
+            });
+        }
+    }
+}
